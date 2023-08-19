4ca16a23fed38f635d40e2ad845355748226faa6
isaac545454 <isaac.newton1221@gmail.com>
2023-08-19 14:00:47 -0300

    refactor: add font and colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <header>header</header>;
+  return (
+    <header className="bg-myGray w-full  flex justify-center items-center py-8">
+      <p className="text-myOrange font-bold">{"header"}</p>
+    </header>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        myOrange: "#ee6c4d",
+        myGray: "#293241",
+        blue100: "#e0fbfc",
+        blue200: "#98c1d9", 
+        blue300: "#3d5a80"
+      },
+   
+    },
   },
   plugins: [],
 }
