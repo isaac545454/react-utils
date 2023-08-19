b52e55325b44c9dda314892af91c537331204ed5
isaac545454 <isaac.newton1221@gmail.com>
2023-08-19 12:29:39 -0300

    chore: add @hookform/error-message

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import React, { InputHTMLAttributes, forwardRef } from "react";
+import { ErrorMessage } from "@hookform/error-message";
 
-export default function Input() {
-  return <div>index</div>;
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  error?: React.ComponentProps<typeof ErrorMessage>;
+  errorExists: boolean;
 }
+
+export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const { errorExists = false, error, ...res } = props;
+
+  return (
+    <div>
+      <input
+        ref={ref}
+        {...res}
+        className={`mb-4 h-10 rounded-lg bg-dark-900 text-white p-4 border
+      border-gray-200 placeholder:text-gray-400 w-full`}
+      />
+      {errorExists && <ErrorMessage {...error} />}
+    </div>
+  );
+});
