e04b76cc1197d5d5d02b0f3e1a6ff5c48244ae6c
isaac545454 <isaac.newton1221@gmail.com>
2023-08-26 13:33:13 -0300

    chore: add jest

--- src/components/Error/error.spec.tsx
+++ src/components/Error/error.spec.tsx
@@ -1,6 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import { describe, test, expect } from "@jest/globals";
 import Error from "./index";
 
 describe("test", () => {
