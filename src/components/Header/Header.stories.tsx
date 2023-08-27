158a5a5d6523b6237c8c97405e454b68853bedb2
isaac545454 <isaac.newton1221@gmail.com>
2023-08-27 10:14:11 -0300

    docs: addd header.stories

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,11 @@
+import Header from ".";
+import type { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  title: "Components/Header",
+  component: Header,
+} as Meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const HeaderPage: Story = {};
