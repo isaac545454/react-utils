f340daeb79ba8af6c6ec88aa3aea254d404fcc64
isaac545454 <isaac.newton1221@gmail.com>
2023-09-05 05:49:55 -0300

    refactos: add hooks index

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useDelete } from "./useDelete";
+export { useGet } from "./useGet";
+export { usePost } from "./usePost";
+export { usePut } from "./usePut";
+export { useStorage } from "./useStorage";
+export { useTreatmentRequest } from "./useTreatmentRequest";
