2c4c9271f73307cadbb0dfd6bc2a0b432f8d2c78
isaac545454 <isaac.newton1221@gmail.com>
2023-10-06 16:23:10 -0300

    feat: add image

--- src/presentation/components/Modal/components/Container/index.tsx
+++ src/presentation/components/Modal/components/Container/index.tsx
@@ -1,16 +1,17 @@
-interface Props {
-  children: JSX.Element;
-  showModal: boolean;
+import { HTMLAttributes } from 'react'
+type Props = {
+	children: JSX.Element
+	showModal: boolean
+} & HTMLAttributes<HTMLDivElement>
+
+export const Container: React.FC<Props> = ({ children, showModal, ...res }) => {
+	if (!showModal) return null
+
+	return (
+		<div className="fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-50" {...res}>
+			<div className="bg-white  rounded-3xl pb-4  relative shadow-md w-1/4 ">
+				<div className="">{children}</div>
+			</div>
+		</div>
+	)
 }
-
-export const Container: React.FC<Props> = ({ children, showModal }) => {
-  if (!showModal) return null;
-
-  return (
-    <div className="fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-50">
-      <div className="bg-white  rounded-3xl pb-4  relative shadow-md w-1/4 ">
-        <div className="">{children}</div>
-      </div>
-    </div>
-  );
-};
--- src/presentation/pages/accessibility/index.tsx
+++ src/presentation/pages/accessibility/index.tsx
@@ -1,4 +1,6 @@
 import { FormEvent, useId } from 'react'
+import { Modal } from '../../components/Modal'
+import { Link } from 'react-router-dom'
 
 export function accessibility() {
 	const id = useId()
@@ -8,27 +10,41 @@ export function accessibility() {
 	}
 
 	return (
-		<div>
+		<>
 			<header>
 				<nav>
-					<li>acessar</li>
+					<Link to="/abolt" aria-label="Clique para acessar a página Sobre">
+						acessar
+					</Link>
 				</nav>
 			</header>
 			<main>
 				<form onSubmit={onSubmit}>
-					<img src="https://miro.medium.com/v2/resize:fit:2944/1*ymbnPv2w9jxDxxS5SQAkTQ.png" alt="clean architecture" />
-
-					<label htmlFor={id}>
-						<input type="text" id={id} className="focus:bg-red-300" />
-					</label>
+					<img
+						src="imagem.jpg"
+						alt="Pintura abstrata"
+						aria-label="Pintura abstrata criada por John Doe em 2023"
+						decoding="sync"
+						onError={e => {
+							e.target.src = 'caminho_da_imagem_de_substituicao.jpg'
+							e.target.alt = 'Imagem de substituição'
+						}}
+					/>
+					<label htmlFor={id}>Digite um texto:</label>
+					<input type="text" id={id} className="focus:bg-red-300" />
 
 					<button aria-label="Adicionar ao carrinho">ADICIONAR</button>
 				</form>
-				<a href="#" className="focus:text-green-600">
+
+				<a href="#" aria-label="Link para algum lugar" className="focus:text-green-600">
 					Link
 				</a>
+				<p id="titulo-modal">Título da Janela Modal</p>
+				<Modal.Container aria-labelledby="titulo-modal" showModal={false}>
+					<div></div>
+				</Modal.Container>
 			</main>
 			<footer>footer</footer>
-		</div>
+		</>
 	)
 }
