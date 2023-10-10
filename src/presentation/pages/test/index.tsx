360db408e73d75730e9cf839b154efa6586f5ff6
isaac545454 <isaac.newton1221@gmail.com>
2023-10-10 17:12:52 -0300

    feat: details

--- src/presentation/pages/test/index.tsx
+++ src/presentation/pages/test/index.tsx
@@ -1,15 +1,21 @@
-export function Test() {
+export function DataList() {
 	return (
 		<div>
-			<label htmlFor="ice-cream-choice">Choose a flavor:</label>
-			<input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
-			<datalist id="ice-cream-flavors">
-				<option value="Chocolate" />
-				<option value="Coconut" />
-				<option value="Mint" />
-				<option value="Strawberry" />
-				<option value="Vanilla" />
-			</datalist>
+			<div>
+				<label htmlFor="ice-cream-choice">Choose a flavor:</label>
+				<input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
+				<datalist id="ice-cream-flavors">
+					<option value="Chocolate" />
+					<option value="Coconut" />
+					<option value="Mint" />
+					<option value="Strawberry" />
+					<option value="Vanilla" />
+				</datalist>
+			</div>
+			<details>
+				<summary>Alguns detalhes</summary>
+				<p>Mais informações sobre os detalhes.</p>
+			</details>
 		</div>
 	)
 }
