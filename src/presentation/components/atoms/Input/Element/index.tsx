761b7764d51d5ff86b659baaa97e83c5cbc1f340
Isaac1221 <isaac.newton1221@gmail.com>
2023-11-24 11:47:13 -0300

    feat: test

--- src/presentation/components/atoms/Input/Element/index.tsx
+++ src/presentation/components/atoms/Input/Element/index.tsx
@@ -1,9 +1,9 @@
-import { forwardRef } from "react";
-import { InputProps } from "../types";
-import { styles } from "../styles";
+import { forwardRef } from 'react'
+import { InputProps } from '../types'
+import { styles } from '../styles'
 
 export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  const { ...res } = props;
+	const { ...res } = props
 
-  return <input ref={ref} {...res} className={styles.Input} />;
-});
+	return <input ref={ref} {...res} className={styles.Input} />
+})
--- src/presentation/utils/file.ts
+++ src/presentation/utils/file.ts
@@ -1,35 +1,63 @@
-export const fileGenerator = (type: string, params: FileParams) => {
-	if (type === 'word') {
-		console.log('word')
-		//code
-	}
-	if (type === 'pdf') {
-		console.log('pdf')
-	}
+const test1 = Object.freeze({
+	test: '/test',
+	login: '/login',
+	users: '/users',
+	test2: { a: 'a' },
+})
+// const test1: Readonly<{
+// 	test: '/test'
+// 	login: '/login'
+// 	users: '/users'
+// 	test2: { a: string }
+// }>
+
+const test2 = {
+	test: '/test',
+	login: '/login',
+	users: '/users',
+	test2: { a: 'a' },
+} as const
+// const test2: {
+// 	readonly test: '/test'
+// 	readonly login: '/login'
+// 	readonly users: '/users'
+// 	readonly test2: { readonly a: 'a' }
+// }
+
+test1.login = '/test2'
+//Cannot assign to 'login' because it is a read-only property.
+test1.test2.a = 'b'
+
+test2.login = '/test2'
+//Cannot assign to 'login' because it is a read-only property.
+test2.test2.a = 'c'
+//Cannot assign to 'a' because it is a read-only property.
+
+type TestProps = (typeof test2)[keyof typeof test2]
+//type TestProps = "/test" | "/login" | "/users"
+
+export enum FileType {
+	WORD = 'word',
+	PDF = 'pdf',
 }
 
-// export enum FileType {
-// 	WORD = 'word',
-// 	PDF = 'pdf',
-// }
+const fileGeneratorWord = ({ test }: FileParams) => console.log(test)
+const fileGeneratorPdf = ({ test }: FileParams) => console.log(test)
 
-// const fileGeneratorWord = ({ test }: FileParams) => console.log(test)
-// const fileGeneratorPdf = ({ test }: FileParams) => console.log(test)
-
-// const fileGeneratorsMap = {
-// 	[FileType.WORD]: fileGeneratorWord,
-// 	[FileType.PDF]: fileGeneratorPdf,
-// }
+const fileGeneratorsMap = {
+	[FileType.WORD]: fileGeneratorWord,
+	[FileType.PDF]: fileGeneratorPdf,
+}
 
 type FileParams = {
 	test: string
 }
 
-// type FileGeneratorParams = {
-// 	type: FileType
-// 	params: FileParams
-// }
+type FileGeneratorParams = {
+	type: FileType
+	params: FileParams
+}
 
-// export const fileGenerator = ({ params, type }: FileGeneratorParams) => {
-// 	return fileGeneratorsMap[type](params)
-// }
+export const fileGenerator = ({ params, type }: FileGeneratorParams) => {
+	return fileGeneratorsMap[type](params)
+}
