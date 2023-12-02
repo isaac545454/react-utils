9be9b69ee6ac07af4d48618831750e2607978e78
Isaac1221 <isaac.newton1221@gmail.com>
2023-12-02 12:55:18 -0300

    feat: test

---
+++ src/presentation/utils/TestFile/index.tsx
@@ -0,0 +1,13 @@
+// const Component = () => {
+// 	return <div>Test</div>
+// }
+
+// export default Component
+
+// import Test from '/components/TestFile'
+
+// export const Component = () => {
+// 	return <div>Test</div>
+// }
+
+// import { Component } from '@/components/TestFile'
