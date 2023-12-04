94b7b9e12108d9d19515108d63220063bb240187
Isaac1221 <isaac.newton1221@gmail.com>
2023-12-04 17:55:36 -0300

    feat: add test

--- src/presentation/components/atoms/Input/Element/index.tsx
+++ src/presentation/components/atoms/Input/Element/index.tsx
@@ -1,9 +1,19 @@
-import { forwardRef } from 'react'
-import { InputProps } from '../types'
+import { ChangeEvent, InputHTMLAttributes, forwardRef } from 'react'
+// import { InputProps } from '../types'
 import { styles } from '../styles'
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-	const { ...res } = props
+type TestProps = {
+	value?: string
+	onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+	disabled: boolean
+}
 
-	return <input ref={ref} {...res} className={styles.Input} />
+export const Test = ({ onChange, value, disabled }: TestProps) => {
+	return <Input onChange={onChange} value={value} disabled={disabled} />
+}
+
+export type InputProps = InputHTMLAttributes<HTMLInputElement>
+
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+	return <input ref={ref} {...props} className={styles.Input} onChange={e => e} />
 })
