f45168570d5fbe3929c82bc251437c3facd125ed
Isaac1221 <isaac.newton1221@gmail.com>
2023-12-10 20:41:15 -0300

    feat: cal

---
+++ src/presentation/utils/cal.spec.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker'
+
+export type Soma = {
+	y: number
+	x: number
+}
+
+export const soma = ({ x, y }: Soma): number => {
+	return x + y
+}
+
+test('soma', () => {
+	const numbers = {
+		x: faker.number.int(),
+		y: faker.number.int(),
+	}
+	const result = soma(numbers)
+	expect(result).toBe(numbers.x + numbers.y)
+})
