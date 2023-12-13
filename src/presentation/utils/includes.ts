52f4ec67efdc27b9548cafbe9485d30cc0250ed3
Isaac1221 <isaac.newton1221@gmail.com>
2023-12-12 21:41:10 -0300

    feat: test

--- src/presentation/utils/includes.ts
+++ src/presentation/utils/includes.ts
@@ -10,11 +10,17 @@
 // }
 
 //- 2
-const PROJECT_STATUS = ['aprovado', 'reprovado', 'pendente'] as const
+const PROJECT_STATUS_OBJ = {
+	aprovado: 'aprovado',
+	reprovado: 'reprovado',
+	pendente: 'pendente',
+} as const
+
+const PROJECT_STATUS: ReadonlyArray<string> = Object.values(PROJECT_STATUS_OBJ)
+
 type ProjectStatus = (typeof PROJECT_STATUS)[number]
 
 const verification = (ProjectProgressStatus: ProjectStatus): void => {
-	if (PROJECT_STATUS.includes(ProjectProgressStatus)) {
-		console.log('contem status')
-	}
+	if (!PROJECT_STATUS.includes(ProjectProgressStatus)) return console.log('não contém status')
+	if (ProjectProgressStatus === PROJECT_STATUS_OBJ.aprovado) return console.log('aprovado')
 }
