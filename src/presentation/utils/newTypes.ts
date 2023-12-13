f51dae0afb092dace236f575b5a609c6c7355bd2
Isaac1221 <isaac.newton1221@gmail.com>
2023-12-12 21:40:44 -0300

    feat: test

--- src/presentation/utils/newTypes.ts
+++ src/presentation/utils/newTypes.ts
@@ -1,20 +1,34 @@
-export type AccessorMethods<T> = {
-	[property in keyof T as `${'get' | 'set'}${Capitalize<string & property>}`]: any
-}
-
-export class UserEmail implements AccessorMethods<Omit<User, 'name'>> {
-	getEmail(): string {
-		return 'aaaa'
-	}
-	setEmail(): string {
-		return 'bbbb'
-	}
-}
-
 export type User = {
 	name: string
 	email: string
 }
+export type handle = User & `on${string}`
+
+export type AccessorMethods<T> = {
+	[property in keyof T as `${'get' | 'set'}${Capitalize<string & property>}`]: any
+}
+//type AccessorMethods<T> = { [property in keyof T as
+//`get${Capitalize<string & property>}` | `set${Capitalize<string & property>}`]: any; }
+
+export type AccessorMethodsTest = AccessorMethods<Omit<User, 'name'>>
+//type AccessorMethodsTest = {
+//     getEmail: any;
+//     setEmail: any;
+// }
+
+export type Obj = Record<handle, () => void>
+// type Obj = {
+//     onfullscreenchange: () => void;
+//     onfullscreenerror: () => void;
+//     onabort: () => void;
+//     onanimationcancel: () => void;
+//     onanimationend: () => void;
+//     onanimationiteration: () => void;
+//     onanimationstart: () => void;
+//     onauxclick: () => void;
+//     onbeforeinput: () => void;
+//     ... 87 more ...;
+//     onwheel: () => void;
 
 const initializeAsyncOperation = async (params: User): Promise<User> => {
 	return new Promise<User>(resolve => {
