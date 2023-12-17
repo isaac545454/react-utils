a2bdf35ba20611bdbd3d67da291d62eb3ad79ff6
Isaac1221 <isaac.newton1221@gmail.com>
2023-12-17 11:40:28 -0300

    feat: exempla

---
+++ src/presentation/utils/function_p.ts
@@ -0,0 +1,11 @@
+export const parseFormat = () => {}
+export const b = () => {}
+export const c = () => {}
+export const d = () => {}
+
+const handle = () => {
+	const formatData = parseFormat()
+	b()
+	c()
+	d()
+}
